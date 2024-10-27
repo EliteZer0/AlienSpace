f12c85b35e0354007cfc99d99528b4b5ae26ddab
DianaYYJeong <Diana.yy.Jeong@gmail.com>
2024-10-27 18:31:03 +0900

    feat: 프로젝트 세팅
    디스코드 채널 Ailien Space 사용자들을 위한 게시판 서비스 프로젝트 진행을 위한 초기 세팅

---
+++ src/main/java/com/aillien/space/Ailienspace1Application.java
@@ -0,0 +1,13 @@
+package com.aillien.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ailienspace1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ailienspace1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/aillien/space/Ailienspace1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aillien.space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ailienspace1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
